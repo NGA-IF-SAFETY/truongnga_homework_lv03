af2f4fc08b05a2171fac6168361464556e066f67
NGA-IF-SAFETY <ngatruong01021998@gmail.com>
2016-10-27 16:36:37 +0700

    Create 04_soChanNhoHonN.c

---
+++ 04_soChanNhoHonN.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+ main(void)
+
+{
+
+ int i,n;
+
+ printf("moi ban nhap so n \n");
+
+ scanf("%d",&n);
+ 
+    for (i=0;i<=n;i+=2)
+
+ printf("so chan nho hon n la: %d\n",i);
+ 
+}
