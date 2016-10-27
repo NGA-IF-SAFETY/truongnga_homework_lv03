e5a102f47c7ce98c56998bb57707e28abd069c07
NGA-IF-SAFETY <ngatruong01021998@gmail.com>
2016-10-27 15:46:51 +0700

    Create 02_tenBanNLan.c

---
+++ 02_tenBanNLan.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#include <conio.h>
+
+ void main()
+ 
+ {
+ 
+  int n,i;
+  
+  printf("nhap so lan xuat \n");
+ 
+  scanf("%d",&n);
+ 
+ for (i=0;i<n;i++)
+ 
+    printf(" truong thi nga \n");
+    
+
+  }
